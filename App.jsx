a13145263974870f3841421dbec20f42bd8a3867
Your Name <your.email@example.com>
2025-03-25 03:08:03 -0700

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>hi my name is zeeshan </Text>
+      <Text>hi my name is zeeshan </Text>
+      <Text>hi my name is zeeshan </Text>
+      <Text>hi my name is zeeshan </Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
